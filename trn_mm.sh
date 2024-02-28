a1e73131d3939cbb611fb6616886861d3028d15e
haluptzok <haluptzok@users.noreply.github.com>
2024-02-27 16:48:49 -0800

    simple script to train fast adam vs sophia

--- trn_mm.sh
+++ trn_mm.sh
@@ -1,2 +1,2 @@
-python makemore.py --gentext=1 --block_size=32 --learning-rate=0.001 --device=cuda --max-steps=100 --optimizer=adam
-python makemore.py --gentext=1 --block_size=32 --learning-rate=0.001 --device=cuda --max-steps=100 --optimizer=sophia
+python makemore.py --gentext=1 --block_size=32 --learning-rate=0.001 --device=cuda --max-steps=5000 --optimizer=adam
+python makemore.py --gentext=1 --block_size=32 --learning-rate=0.001 --device=cuda --max-steps=5000 --optimizer=sophia
